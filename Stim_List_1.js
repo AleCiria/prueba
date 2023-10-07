a1a753299b78f75cd3fbd32e1c2c209bc8c841a6
AleCiria <ciriacontacto@gmail.com>
2023-10-07 13:09:03 -0600

    Add files via upload

---
+++ Stim_List_1.js
@@ -0,0 +1,18 @@
+var Stim_List_1= [
+
+  {stimulus: ["airplane_interior_A.jpg","airplane_interior_A_CON.jpg"],
+   correct_response: "v",
+   con: "v",
+   inc: "n",
+   tipo: "1"
+},
+
+  {stimulus: ["art_gallery_A.jpg","art_gallery_A_INC.jpg"],
+   correct_response: "n",
+   con: "v",
+   inc: "n",
+   tipo: "2"
+}
+
+]
+
---
+++ Stim_List_2.js
@@ -0,0 +1,19 @@
+var Stim_List_2= [
+
+  {stimulus: ["airplane_interior_A.jpg","airplane_interior_A_CON.jpg"],
+   correct_response: "n",
+   con: "n",
+   inc: "v",
+   tipo:"1"
+},
+
+  {stimulus: ["art_gallery_A.jpg","art_gallery_A_INC.jpg"],
+   correct_response: "v",
+   con: "n",
+   inc: "v",
+   tipo:"2"
+}
+
+]
+
+
